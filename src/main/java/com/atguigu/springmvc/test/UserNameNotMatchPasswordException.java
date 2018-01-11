e71382da649b246f3bd30a5e3981690730949adb
plm938 <25652061@qq.com>
2018-01-11 15:23:54 +0800

    test

--- src/main/java/com/atguigu/springmvc/test/UserNameNotMatchPasswordException.java
+++ src/main/java/com/atguigu/springmvc/test/UserNameNotMatchPasswordException.java
@@ -3,7 +3,7 @@ package com.atguigu.springmvc.test;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value=HttpStatus.FORBIDDEN, reason="sdd!")
+@ResponseStatus(value=HttpStatus.FORBIDDEN, reason="用户名和密码不匹配!")
 public class UserNameNotMatchPasswordException extends RuntimeException{
 
 
